8612099225aa86a51f34f90616645fc9f3f235f6
DoanTheTung <doanthetung126@gmail.com>
2024-11-24 20:00:48 +0700

    upload bai 4

---
+++ ss4_bai1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	//khai bao so nguyen can nhap
+	int soNguyen;
+	//nhap so nguyen can kiem tra 
+	printf ("nhap so nguyen can kiem tra : ");
+	scanf("%d", &soNguyen);
+	//kiem tra va in ra man hinh ket qua
+	if(soNguyen > 0){
+		printf("so %d là so nguyen duong.\n", soNguyen );
+    } else if (soNguyen < 0) {
+        printf("so %d là so nguyen am.\n", soNguyen);
+    } else {
+        printf("so vua nhap la so 0 (không âm, không dýõng).\n");
+    }
+
+    return 0;
+	} 
+	 
+
